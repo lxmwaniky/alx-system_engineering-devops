e53b050b8743a565a42a14dbc3d71362e62a5421
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-22 14:54:27 +0300

    Add script to gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+returns information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        userId = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, userId))
+        employeeName = req.json().get("name")
+        if employeeName is not None:
+            treq = requests.get("{}todos?userId={}".format(url, userId)).json()
+            totalTaskCount = len(treq)
+            doneTasks = []
+            for i in treq:
+                if i.get("completed") is True:
+                    doneTasks.append(i)
+            doneTasksCount = len(doneTasks)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(employeeName, doneTasksCount, totalTaskCount))
+            for title in doneTasks:
+                print("\t{}".format(title.get("title")))
